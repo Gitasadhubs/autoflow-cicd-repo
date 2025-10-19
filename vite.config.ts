25de482aceb67476cab81422a84578b436c7e853
Asad Khan <asadkhan1932001@gmail.com>
2025-10-19 15:39:28 +0500

    feat: Exclude prismjs from Vite build
    To prevent potential build issues and reduce bundle size, prismjs and its components are now marked as external dependencies in the Vite configuration. This ensures they are not bundled with the application.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        'react-simple-code-editor',
+        'prismjs',
+        /^prismjs\/.*/, // To handle imports like 'prismjs/components/prism-yaml.js'
+      ],
+    },
+  },
   server: {
     // This proxy is for local development to forward API requests
     // to the Express server running on port 3001.
